afe169061f09dbe46b4cc30b6d8b4ce7c89bb7e8
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-06-25 20:05:00 +0300

    Model is Working

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const{Schema,model}=require('mongoose')
+
+const schema=new Schema({
+    Adres: {type: String, required: true, unique: false},
+    Price: {type: Number, required: true, unique: false},
+    IMG: {type: String, required: true, unique: false},
+    RommsCount:{type: Number, required: true, unique: false},
+    Square:{type: Number, required: true, unique: false},
+    Floor:{type: Number, required: true, unique: false}
+})
+
+module.exports=model('Product',schema)
--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,29 @@
 const {Router} = require('express')
+const Product= require('../models/Product')
 const router = Router()
 router.get('/',async(req, res)=>{
-    res.render('index', {title: 'ZohaDom'})
+    const products= await Product.find({})
+    res.render('index', {title: 'ZohaDom', products})
 })
 
 router.get('/admin',async(req, res)=>{
-    res.render('admin', {title: 'ZohaDom'})
+    res.render('admin', {title: 'Admin Page'})
 })
+
+router.post('/createproduct',async(req, res)=>{
+    const product = new Product(
+    {
+        Adres: req.body.Adres,
+        Price: req.body.Price,
+        IMG: req.body.IMG,
+        RommsCount: req.body.RommsCount,
+        Square: req.body.Square,
+        Floor: req.body.Floor
+    }) 
+    await product.save()
+    res.redirect('/')
+})
+
+
+
 module.exports = router
