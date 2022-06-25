c7a491deae6792c7ddcbd4388f0886a10c677cc2
zak10110 <35228898+zak10110@users.noreply.github.com>
2022-06-25 16:25:17 +0300

    project eto basa is ready

--- routes/home.js
+++ routes/home.js
@@ -3,4 +3,8 @@ const router = Router()
 router.get('/',async(req, res)=>{
     res.render('index', {title: 'ZohaDom'})
 })
+
+router.get('/admin',async(req, res)=>{
+    res.render('admin', {title: 'ZohaDom'})
+})
 module.exports = router
